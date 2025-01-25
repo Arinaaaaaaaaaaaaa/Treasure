f93e93d5ecfcfe7b7058ff92308865af61e31c8c
Arina <horbunovaarina@gmail.com>
2025-01-25 15:20:46 +0200

    .

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ const treasure = {
     const coords = document.getElementById("coords");
     let distanceX = Math.abs(event.offsetX - treasure.x);
     let distanceY = Math.abs(event.offsetY - treasure.y);
-    let x = Math.sqrt(Math.pow(distanceX, 2) - Math.pow(distanceY, 2));
+    let dis = Math.pow(distanceX, 2) + Math.pow(distanceY, 2)
+    let c = Math.sqrt(dis);
+    let x = event.offsetX
     //... // тут отримуємо значення координат миші x та y від події event
     coords.value = `Координати: ${x}`;
-    if (x < 30) {
+    if (c < 30) {
       // умова, де знаходиться скарб - відстань від координат курсору до координат скарбу менше 30
       alert("Скарб тут!");
     }
